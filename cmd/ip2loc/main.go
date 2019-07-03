f5a7f539744ce942873a7985d4789fcccd9be1be
hwchan <honwen.chan@gmail.com>
2019-07-04 01:34:52 +0800

    new data format

---
+++ cmd/ip2loc/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/chenhw2/ip2loc"
+)
+
+func stdin() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		ip := scanner.Text()
+		if loc, err := ip2loc.IP2loc(ip); err != nil {
+			fmt.Fprintf(os.Stderr, "%s: %v\n", ip, err)
+		} else {
+			fmt.Println(ip, loc.CountryName, loc.RegionName, loc.CityName)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "Failed:", err)
+		os.Exit(1)
+	}
+
+	return
+}
+
+func main() {
+	if len(os.Args) > 1 {
+		if loc, err := ip2loc.IP2loc(os.Args[1]); err != nil {
+			fmt.Fprintf(os.Stderr, "%s: %v\n", os.Args[1], err)
+			os.Exit(1)
+		} else {
+			fmt.Println(loc.CountryName, loc.RegionName, loc.CityName)
+		}
+	} else {
+		stdin()
+	}
+}
--- ip2loc.go
+++ ip2loc.go
@@ -4,25 +4,25 @@
 package ip2loc
 
 import (
-	"github.com/chenhw2/ip2loc/datx"
+	"github.com/chenhw2/ip2loc/ipdb"
 	_ "github.com/chenhw2/ip2loc/statik"
 	"github.com/rakyll/statik/fs"
 )
 
-var city *datx.City
+var city *ipdb.City
 
-func IP2loc(ip string) (loc datx.Location, err error) {
+func IP2loc(ip string) (loc *ipdb.CityInfo, err error) {
 	if nil == city {
 		stkfs, err := fs.New()
 		if err != nil {
 			return loc, err
 		}
 
-		file, err := stkfs.Open("/17monipdb.datx")
+		file, err := stkfs.Open("/ipipfree.ipdb")
 		if err != nil {
 			return loc, err
 		}
-		city, err = datx.NewCity(file) // For City Level IP Database
+		city, err = ipdb.NewCity(file) // For City Level IP Database
 	}
-	return city.FindLocation(ip)
+	return city.FindInfo(ip, "CN")
 }
